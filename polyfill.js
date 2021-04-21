12f9763c4b5a6acf56fe919da7b94d85137e3145
Jamie Kyle <me@thejameskyle.com>
2021-04-21 11:53:12 -0700

    fix polyfill for null and undefined objects

--- polyfill.js
+++ polyfill.js
@@ -2,7 +2,10 @@ if (!Object.hasOwn) {
   var hasOwnProperty = Object.prototype.hasOwnProperty
 
   Object.defineProperty(Object, "hasOwn", {
-    value: function(object, property) {
+    value: function (object, property) {
+      if (object == null) {
+        throw new TypeError("Cannot convert undefined or null to object")
+      }
       return hasOwnProperty.call(Object(object), property)
     },
     configurable: true,
