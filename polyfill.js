f3844daf397a66a603bfcdfbaa82cbd38839e3ee
steven nguyen <nguyeste008@students.garlandisd.net>
2021-05-26 00:58:08 -0500

    Fix invalid scoping (#15)
    `var` is function scoped, so `hasOwnProperty` was leaked as a global variable.

--- polyfill.js
+++ polyfill.js
@@ -1,12 +1,10 @@
 if (!Object.hasOwn) {
-  var hasOwnProperty = Object.prototype.hasOwnProperty
-
   Object.defineProperty(Object, "hasOwn", {
     value: function (object, property) {
       if (object == null) {
         throw new TypeError("Cannot convert undefined or null to object")
       }
-      return hasOwnProperty.call(Object(object), property)
+      return Object.prototype.hasOwnProperty.call(Object(object), property)
     },
     configurable: true,
     enumerable: false,
